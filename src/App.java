91cec75bc95d2b3124fd81b7f9c25515baa46428
avergani <81098902+avergani@users.noreply.github.com>
2023-05-13 01:01:35 +0000

    Create an array of double

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
 public class App {
   
   public static void main(String[] args) {
+
+    double[] lotteryNumbers = new double[5];
+
+    System.out.println(lotteryNumbers[2]);
+
+    lotteryNumbers[2] = 40;
+    System.out.println(lotteryNumbers[2]);
+
+    
     
   }
 }
